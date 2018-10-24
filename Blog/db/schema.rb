4dda523865989f8aa9150f8fda55966eb17aa5fb
zOmegad <simonrichard1998@gmail.com>
2018-10-24 14:09:14 +0200

    adding tables

--- Blog/db/schema.rb
+++ Blog/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_083326) do
+ActiveRecord::Schema.define(version: 2018_10_24_120202) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
